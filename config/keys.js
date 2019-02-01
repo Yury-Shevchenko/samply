dacecf561ae5bd311ed1f049995d5d71e5b6aeae
Yury-Shevchenko <shevchenko_yury@mail.ru>
2019-02-01 17:50:30 +0100

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,8 @@
 if(process.env.NODE_ENV == "production"){
   module.exports = require('./keys_prod');
 } else {
-  module.exports = require('./keys_dev');
+  module.exports = {
+    stripePublishableKey: 'pk_test_bQ8jWWd1ktfrrxqxyEzi10jn',
+    stripeSecretKey: 'sk_test_mL40Uasrl81K84eMyk6BB5Ce'
+  };
 }
